aed94eeb2a9949bf45ba80bfe62493883d5eb452
angela-boyadjian <boyadjian.angela@gmail.com>
2019-10-14 03:19:03 +0200

    [ADD] Comment classes

---
+++ epicture/lib/images/commentPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:epicture/model/image.dart';
+import 'package:epicture/model/requests.dart';
+import 'package:epicture/images/photosList.dart';
+
+class CommentPage extends StatefulWidget {
+  ImgurImage photos;
+
+  @override
+  CommentPageState createState() => new CommentPageState();
+}
+
+// add_a_photo 
+class CommentPageState extends State<CommentPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
